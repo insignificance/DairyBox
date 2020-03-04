a8e976c04372a7ef8672b286ccf76968139b3a1a
insignificance <888888@qq.com>
2020-03-04 15:04:58 +0800

    init

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiaryBox
+//
+//  Created by ～^～^～ on 16/5/2.
+//  Copyright © 2016年 ～^～^～. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
